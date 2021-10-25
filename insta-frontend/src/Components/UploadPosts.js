2a2a541ebb47d2996588a226f0aa082283929c07
Gamoundo <akilgg@gmail.com>
2021-10-24 23:46:34 -0400

    added a form to upload post page

--- insta-frontend/src/Components/UploadPosts.js
+++ insta-frontend/src/Components/UploadPosts.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Upload() {
+
+    const [formData, setformData] = useState(
+        {
+            file: "",
+            caption: ""
+        }
+    )
+
+        const handleChange = (e) => {
+            setformData((prevState) => {
+                return{
+                    ...prevState,
+                    [e.target.name]: e.target.value
+                }
+            }
+
+            )
+        }
+
+
+     const   handleFileChange = (e) => {
+                setformData((prevState) => {
+                    return{
+                        ...prevState,
+                        [e.target.name]: e.target.files[0]
+                    }
+                }
+
+                )
+        }
+
     return(
         <div>
             <p>Share a pic</p>
+
+            <form>
+                <div>
+                    <input onChange={handleFileChange} type='file' name='file'/>
+                    
+                </div>
+                <div>
+                    <input onChange={handleChange} value={formData.caption} type='text' name='caption'/>
+                    <label htmlFor="caption">Caption</label>
+                </div>
+                <input type="submit" value="Upload Post"  />
+            </form>
         </div>
     )
 }
