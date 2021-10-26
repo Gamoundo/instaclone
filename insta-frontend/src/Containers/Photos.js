5b5326b1bce7bedea1b1317d97354d424f86bd2a
Gamoundo <akilgg@gmail.com>
2021-10-26 12:47:26 -0400

    made the fetch to posts in photos comp

--- insta-frontend/src/Containers/Photos.js
+++ insta-frontend/src/Containers/Photos.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Photos() {
+
+const [post, setpost] = useState([])
+
+useEffect(() => {
+    fetch("http://localhost:3001/posts")
+    .then(resp => resp.json())
+    .then(setpost)
+}, [])
+console.log(post)
+const displayPosts = (arr) => {
+    return(arr.map((ele) => {
+        return(
+            <div>
+                <img  src={ele.photo} alt={ele.caption} width="200px" height="200px"/>
+                <p>{ele.caption}</p>
+                <p>Likes {ele.likes}</p>
+            </div>
+        )
+    }
+
+    ))
+}
+
     return(
         <div>
             <h1> The Photos</h1>
+            {post.length >=1 && displayPosts(post)}
         </div>
     )
     
